ad35ee6e20c5fee4ae3fca2eb87f865709cd4072
mecca <fnjakai@outlook.com>
2022-07-22 12:41:13 +0300

    Porkie Times: Using hooks and Axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, {Component} from 'react';
 
-const Home = () => {
+class Home extends Component{
+    state = {
+        posts: []
+    }
     
-    return(
-        <div className='container'>
-            <h4 className='center'>Home</h4>
-            <p> We always tell porkies here at Porkie Times</p>
-        </div>
-    );
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            // console.log(res);
+            this.setState({
+                posts: res.data.slice(0,10)
+            })
+        });
+    }
+    
+    render() {
+        const {posts} = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return(
+                    <div className='post card' key={post.id}>
+                        <div className='card-content'>
+                            <span className='card-title'>{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                );
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        );
+
+        return(
+            <div className='container'>
+                <h4 className='center'>Home</h4>
+                <p> We always tell porkies here at Porkie Times</p>
+                {postList}
+            </div>
+        );
+    }
 }
 
 export default Home
